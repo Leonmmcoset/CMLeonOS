6107ecc2db1be3cc2bb4d158fa5e57e339826168
Leonmmcoset <testmod@qq.com>
2026-02-12 10:45:07 +0800

    构建版本2

--- BuildTime.ps1
+++ BuildTime.ps1
@@ -1,3 +1,3 @@
 $buildTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-$buildTime | Out-File -FilePath "BuildTime.txt" -Encoding UTF8
+$buildTime | Out-File -FilePath "BuildTime.txt" -Encoding ASCII -NoNewline
 Write-Host "Build time written to BuildTime.txt: $buildTime"
